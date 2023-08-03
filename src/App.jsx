94f985998d8ab1af9cda1f1a4c3029bb87437a47
ipjayawick <ipjayawick@gmail.com>
2023-08-04 00:19:16 +0530

    Basic Functions working. adding,checking,deleting

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
-import Stack from 'react-bootstrap/Stack';
-import { Card, Form, Container, Row, Col, Navbar, Nav, Button, ListGroup } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { Card, Form, Container, Navbar, Button, Stack } from 'react-bootstrap';
+
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todoList, setTodoList] = useState([])
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTodoList(currentTodoList => {
+      return [
+        ...currentTodoList, { id: crypto.randomUUID(), title: newItem, completed: false }
+      ]
+    })
+    setNewItem("")
+  }
+  function toggleTodo(id, completed) {
+    setTodoList(currentTodoList => {
+      return currentTodoList.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+  function handleDelete(id) {
+    setTodoList(currentTodoList => {
+      return currentTodoList.filter(todo =>{
+        return todo.id !==id
+      })
+    })
+  }
   return (
     <>
-      <Container fluid className=" bg-secondary p-0" style={{
-        height: '100vh', // Set the desired height here
-        backgroundColor: 'lightblue', // (Optional) Set background color
-        // Add other styles as needed
-      }}>
+      <Container fluid className=" bg-secondary p-0" style={{ height: '100vh', backgroundColor: 'lightblue' }}>
         <Navbar bg="dark" variant="dark">
           <Container fluid>
             <Navbar.Brand style={{ fontSize: '25px', fontWeight: 'bold' }}>Todo App</Navbar.Brand>
           </Container>
         </Navbar>
-        <div className='container w-75 py-5 ms-4' style={{maxWidth:'600px'}}>
+
+        <div className='container w-75 py-5 ms-4' style={{ maxWidth: '600px' }}>
           <Card className='bg-light'>
             <Card.Body>
-              <Form>
+              <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="textInput">
                   <Stack gap={3}>
                     <Form.Label className="text-left w-100 h5">Add New Item</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Todo" />
+                    <Form.Control type="text" value={newItem} onChange={e => setNewItem(e.target.value)} placeholder="Enter Todo" />
                     <Button variant="primary form-control" type="submit" className='w-50 ms-auto'>
                       Add
                     </Button>
@@ -34,28 +58,35 @@ export default function App() {
         </div>
         <div>
           <div>
-            <Form className='container w-75 ms-4' style={{maxWidth:'600px'}}>
+            <Form className='container w-75 ms-4' style={{ maxWidth: '600px' }}>
               <div className='h3 mb-2'>Todo List</div>
               <Form.Group>
-                <Stack direction="horizontal" gap={0} >
-                  <Form.Check
-                    type="checkbox"
-                    label=""
-                    className="d-flex align-items-center justify-content-center"
-                  />
-                  <Form.Text className='text-center  my-auto me-2 text-light' style={{
-                    fontSize: '20px', // Set the desired font size here
-                    fontWeight: '', // Set the desired font weight here
-                  }}>
-                    This is a Todo
-                  </Form.Text>
-                  <Button
-                    variant="danger"
-                    className="ml-auto btn-sm "
-                  >
-                    Delete
-                  </Button>
-                </Stack>
+                {todoList.map(todo => {
+                  return <Stack direction="horizontal" gap={0} className='mb-2' key={todo.id}>
+                    <Form.Check
+                      type="checkbox"
+                      label=""
+                      className="d-flex align-items-center justify-content-center"
+                      checked={todo.completed}
+                      onChange={e => {
+                        toggleTodo(todo.id, e.target.checked)
+                      }}
+                    />
+                    <Form.Text className='text-center  my-auto me-2 text-light' style={{ fontSize: '20px' }}>
+                      {todo.title}
+                    </Form.Text>
+                    <Button
+                      variant="danger"
+                      className="ml-auto btn-sm ms-auto me-5"
+                      onClick={()=>handleDelete(todo.id)}
+                    >
+                      Delete
+                    </Button>
+
+                  </Stack>
+
+                })}
+
               </Form.Group>
             </Form>
 
